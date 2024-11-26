68753d24ed9660d4b83e3d4e3b26dc3383e0175b
mahdihaghollahi <mahdiaghollahi@gmail.com>
2024-11-26 20:15:55 +0330

    add filter drop down in product

--- src/Component/Product/FilterData.tsx
+++ src/Component/Product/FilterData.tsx
@@ -1,25 +1,26 @@
 import React, { Fragment } from "react";
+import FilterDropDown from "./FilterDropDown";
 
 function FilterData() {
   return (
     <Fragment>
-      <div className="bg-white w-80 -mt-[72px] py-5 rounded-lg mr-10 h-[10%] shadow">
-       
-            <p className="font-medium text-[#3A3A3A] text-base mr-4" >
-            فیلتر ها
-            </p>
-       
-        <div className="flex flex-col gap-8 px-4 py-4">
-          <label className="flex justify-between items-center gap-3 cursor-pointer">
-            <span className="label-text font-medium text-base text-[#393939]">
-              فقط کالاهای موجود
-            </span>
-            <input
-              type="checkbox"
-              className="toggle border-[#E8EDFE]   bg-[#476FF3] checked:bg-[#476FF3]  checked:[--tglbg:#E8EDFE]  hover:bg-[#476FF3]"
-              defaultChecked
-            />
-          </label>
+      <div className="flex flex-col items-center">
+        <div className="bg-white w-80  py-5 rounded-lg mr-10  shadow">
+          <div className="flex flex-col gap-8 px-4 py-4">
+            <label className="flex justify-between items-center gap-3 cursor-pointer">
+              <span className="label-text font-medium text-base text-[#393939]">
+                فقط کالاهای موجود
+              </span>
+              <input
+                type="checkbox"
+                className="toggle border-[#E8EDFE]   bg-[#476FF3] checked:bg-[#476FF3]  checked:[--tglbg:#E8EDFE]  hover:bg-[#476FF3]"
+                defaultChecked
+              />
+            </label>
+          </div>
+        </div>
+        <div className="">
+          <FilterDropDown />
         </div>
       </div>
     </Fragment>
