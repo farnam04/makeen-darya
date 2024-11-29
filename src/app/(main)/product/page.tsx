d735dfd454b54e3102456440f4c6d97c95a40cb8
mahdihaghollahi <mahdiaghollahi@gmail.com>
2024-11-29 21:41:43 +0330

    add footer

--- src/app/(main)/product/page.tsx
+++ src/app/(main)/product/page.tsx
@@ -2,12 +2,15 @@ import React, { Fragment } from "react";
 import Header from "@/Component/Landing/Header/Header";
 import Navbar from "@/Component/Landing/Navbar/Navbar";
 import Product from "@/Component/Products/Product/Product";
+import Footer from "@/Component/Landing/Footer/Footer";
 const page = () => {
   return (
     <Fragment>
       <Header />
       <Navbar />
+
       <Product />
+      <Footer />
     </Fragment>
   );
 };
