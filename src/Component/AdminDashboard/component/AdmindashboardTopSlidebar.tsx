456ab0cbd82a3d22abd655f02fae641be6372ed1
farnam04 <far.deh8304@gmail.com>
2025-01-06 11:01:38 -0800

    fix admin dashboard top sildebar

--- src/Component/AdminDashboard/component/AdmindashboardTopSlidebar.tsx
+++ src/Component/AdminDashboard/component/AdmindashboardTopSlidebar.tsx
@@ -1,16 +1,24 @@
 import Logogulf from "@/component/AdminDashboard/Logo/Logogulf";
 import React from "react";
+import Image from "next/image";
+import Logout from "@/assent/Imge/Panel/Logout.svg";
 
 export default function AdmindashboardTopSlidebar() {
   return (
-    <div className=" flex items-center  w-full h-full justify-between  bg-white ">
+    <div className=" flex items-center  w-full h-full justify-between  bg-[#fafafa] ">
       <div className=" w-[300px] h-full flex items-center justify-between text-[14px] ">
         <Logogulf />
         <p>به پنل ادمین خوش آمدید!</p>
       </div>
-      <div className="h-full w-[300px] flex items-center flex-row-reverse " >
-        <button className="w-[50px] h-[80%] bg-red-950  " ></button>
+      <div className="h-full w-[300px] flex items-center flex-row-reverse ">
+        <div className="w-[50px] h-[80%] flex items-center justify-center ">
+          <button>
+            {" "}
+            <Image src={Logout} alt="" width={30} height={24} />
+          </button>
+        </div>
       </div>
     </div>
   );
 }
+
