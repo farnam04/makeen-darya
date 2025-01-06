3dbf748c1c98277c02af2e57b102ee5b68cfb654
farnam04 <far.deh8304@gmail.com>
2025-01-06 11:16:09 -0800

    fix user in admin dashboar link

--- src/app/(panel)/admindashboard/user/page.tsx
+++ src/app/(panel)/admindashboard/user/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import AdmindashboardNavbar from "@/component/AdminDashboard/component/AdmindashboardNavbar";
+import AdmindashboardTopSlidebar from "@/component/AdminDashboard/component/AdmindashboardTopSlidebar";
+import ToplineUserdash from "@/component/AdminDashboard/userpanel/ToplineUserdash";
+import Usersbar from "@/component/AdminDashboard/userpanel/Usersbar";
+import React, { Fragment } from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <Fragment>
+      <div className="w-full h-[1000px] flex items-center justify-center bg-[#f6f6f6]">
+        <div className="flex flex-col  w-[95%] h-full">
+          <div className="w-full h-[70px]">
+            <AdmindashboardTopSlidebar />
+          </div>
+          <div className="w-full h-[930px]   flex justify-between mt-[10px] ">
+            <div className=" h-full w-[180px]">
+              <AdmindashboardNavbar />
+            </div>
+            <div className="flex flex-col h-full w-[1250px] bg-white p-[16px] ">
+<ToplineUserdash/>
+<Usersbar/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
