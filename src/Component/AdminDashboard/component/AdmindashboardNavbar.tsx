4789e2d316a636b3a102a0b6d21fea6dbb1aa085
farnam04 <far.deh8304@gmail.com>
2025-01-06 10:56:05 -0800

    fix admin dashboard navbar

--- src/Component/AdminDashboard/component/AdmindashboardNavbar.tsx
+++ src/Component/AdminDashboard/component/AdmindashboardNavbar.tsx
@@ -1,33 +1,40 @@
 "use client";
-
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { NavData } from "./NavData";
-import { text } from "stream/consumers";
 
 export default function AdmindashboardNavbar() {
   const pathname = usePathname();
 
   return (
-    <div className="w-full h-full bg-white flex flex-col  ">
-      <div className="w-full h-[90%] flex flex-col justify-around text-[#6B6B6B]">
+    <div className="w-full h-full bg-[#fafafa] flex flex-col">
+      <div className="w-full h-[90%] flex flex-col items-center justify-around text-[#6B6B6B] scroll-m-0">
         {NavData.map((item, index) => (
           <Link
             key={index}
             href={item.href}
-            className={`w-full h-[60px] flex flex-row-reverse items-center justify-end pr-[20px] text-[16px] ${
-              pathname === item.href ? "bg-[#1235AA]" : ""
-            }`}
+            className={`${
+              pathname === item.href
+                ? "bg-[#e8edfe] w-[90%] h-[55px] flex flex-row-reverse items-center justify-end pr-[40px] text-[16px] rounded-lg"
+                : "w-full h-[60px] flex flex-row-reverse items-center justify-end pr-[20px] text-[16px] hover:translate-x-[-10px]"
+            } 
+            sm:w-[90%] sm:h-[50px] lg:w-[95%] lg:h-[55px]`} 
           >
             <div className="w-full h-full flex items-center text-[16px] text-[#565656]">
-              <Image src={item.img} alt={item.name} width={24} height={24} />
+              <Image
+                src={item.img}
+                alt={item.name}
+                width={24}
+                height={24}
+                className={`${pathname === item.href ? "filter grayscale-0" : "filter grayscale"}`}
+              />
               <p
-                className={`mr-3 ${
-                  pathname === item.href ? "text-white" : ""
-                }`}
-              >
+                className={`mr-3 text-sm sm:text-xs lg:text-base ${
+                  pathname === item.href ? "text-black" : ""
+                }`} 
+              > 
                 {item.name}
               </p>
             </div>
