4ed0c2bd6ddfe0716e48c74c2c0ffc8fd9985a23
farnam04 <far.deh8304@gmail.com>
2025-01-09 06:30:32 -0800

    userlistNOTcompelet

---
+++ src/Component/AdminDashboard/userpanel/UserbarData.tsx
@@ -0,0 +1,89 @@
+
+export const UserBarData = [
+  {
+    namde: "jasem",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },
+  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },
+  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "hasan",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },  {
+    namde: "qasem",
+    number: "091076774041",
+    orderNum: "45",
+    position: "co-leader",
+    Lastseen: "a loong time ago",
+  },
+];
+
+
+
+
+  
--- src/app/(panel)/admindashboard/orders/page.tsx
+++ src/app/(panel)/admindashboard/orders/page.tsx
@@ -1,10 +1,24 @@
-import AdmindashboardNavbar from '@/component/AdminDashboard/component/AdmindashboardNavbar'
-import React from 'react'
+import AdmindashboardNavbar from "@/component/AdminDashboard/component/AdmindashboardNavbar";
+import AdmindashboardTopSlidebar from "@/Component/AdminDashboard/component/AdmindashboardTopSlidebar";
+import Mainpage from "@/Component/AdminDashboard/Dashboard/Mainpage";
+import React, { Fragment } from "react";
 
 export default function page() {
   return (
-    <div>
-      <AdmindashboardNavbar/>
-    </div>
-  )
+    <Fragment>
+      <div className="w-full h-[1000px] flex items-center justify-center bg-[#f6f6f6]">
+        <div className="flex flex-col  w-[95%] h-full">
+          <div className="w-full h-[70px]">
+            <AdmindashboardTopSlidebar />
+          </div>
+          <div className="w-full h-[930px]   flex justify-between mt-[10px] ">
+            <div className=" h-full w-[180px]">
+              <AdmindashboardNavbar />
+            </div>
+            <div className=" h-full w-[1250px] bg-white "></div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
