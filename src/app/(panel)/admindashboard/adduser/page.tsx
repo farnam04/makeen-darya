0d1301720133e7eada1125cc71b41b2f60d9b9c7
farnam04 <far.deh8304@gmail.com>
2025-01-10 15:36:40 -0800

    addNewUser

---
+++ src/app/(panel)/admindashboard/adduser/page.tsx
@@ -0,0 +1,23 @@
+import NewUserForm from '@/Component/AdminDashboard/adduser/NewUserForm'
+import AdmindashboardNavbar from '@/Component/AdminDashboard/component/AdmindashboardNavbar'
+import AdmindashboardTopSlidebar from '@/Component/AdminDashboard/component/AdmindashboardTopSlidebar'
+import React, { Fragment } from 'react'
+
+export default function page() {
+  return (
+    <Fragment>
+      <div className="w-full h-[1000px] flex items-center justify-center bg-[#f6f6f6]">
+        <div className="flex flex-col  w-[95%] h-full">
+          <div className="w-full h-[70px]">
+            <AdmindashboardTopSlidebar />
+          </div>
+          <div className="w-full h-[930px]   flex justify-between mt-[10px] ">
+            <div className=" h-full w-[180px]">
+              <AdmindashboardNavbar />
+            </div>
+            <div className=" h-full w-[1250px] bg-white "><NewUserForm/></div>
+          </div>
+        </div>
+      </div>
+    </Fragment>  )
+}
